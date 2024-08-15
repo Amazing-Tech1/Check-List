601001b1c6a64284cd915c6e8638874893ba6d16
Makinde Kelvin O <kelvinmakinde2@gmail.com>
2024-08-15 10:29:07 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'my-custom-dist'}
+    outDir: 'my-custom-dist',
+    index: 'index.html'}
 })
